c4df2a9150d1fcfb983bc6fb22c56bc38e9e9be0
Sviatlana Beliai <sviatlana_beliai@epam.com>
2017-01-04 10:33:25 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Main {
+
+    public static void main(String[] args) {
+       /* WebDriver driver = new FirefoxDriver();
+        driver.get("https://vk.com/");
+        driver.findElement(By.id("index_email")).sendKeys("svetlana.belyai@gmail.com");
+        driver.findElement(By.id("index_pass")).sendKeys("12345");*/
+        System.out.println("Hello");
+
+    }
+}
+
